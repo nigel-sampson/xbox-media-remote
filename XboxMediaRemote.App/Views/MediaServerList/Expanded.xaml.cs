5a60ed017f9f2766170a1b9ba753093f191acecf
Nigel Sampson <nigel.sampson@compiledexperience.com>
2013-12-12 15:28:16 +1300

    Context Views

---
+++ XboxMediaRemote.App/Views/MediaServerList/Expanded.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XboxMediaRemote.App.Views.MediaServerList
+{
+    public sealed partial class Expanded
+    {
+        public Expanded()
+        {
+            InitializeComponent();
+        }
+    }
+}
